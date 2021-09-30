7aabdc899887a118378eb9779e01e9f4ef9867f0
code-king1 <66792586+code-king1@users.noreply.github.com>
2021-09-30 12:56:49 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a>Theatre Assistant<b> &nbsp; Admin </b></a>
+    <a>Theatre Assistant<b> &nbsp; &nbsp; Admin </b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
